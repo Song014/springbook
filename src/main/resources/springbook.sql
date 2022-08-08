1b1f2aa135ad76522f07c5d1217126965034c4cb
Song014 <ghgsb6200@gmail.com>
2022-08-08 12:08:22 +0900

    sql 파일 추가

---
+++ src/main/resources/springbook.sql
@@ -0,0 +1,53 @@
+CREATE TABLE users (
+    id       VARCHAR2(8 BYTE),
+    password VARCHAR2(8 BYTE),
+    name     VARCHAR2(20 BYTE),
+    role     VARCHAR2(5 BYTE),
+    PRIMARY KEY ( id )
+);
+
+INSERT INTO users VALUES (
+    'test',
+    'test1234',
+    '관리자',
+    'Admin'
+);
+
+INSERT INTO users VALUES (
+    'user1',
+    'user1',
+    '홍길동',
+    'User'
+);
+
+SELECT
+    *
+FROM
+    users;
+
+CREATE TABLE board (
+    seq     NUMBER(5, 0),
+    title   VARCHAR2(200 BYTE),
+    writer  VARCHAR2(20 BYTE),
+    content VARCHAR2(2000 BYTE),
+    regdate DATE DEFAULT sysdate + 1,
+    cnt     NUMBER(5, 0) DEFAULT 0,
+    PRIMARY KEY ( seq )
+);
+
+INSERT INTO board (
+    seq,
+    title,
+    writer,
+    content
+) VALUES (
+    1,
+    '가입인사',
+    '관리자',
+    '잘부탁합니다...'
+);
+
+SELECT
+    *
+FROM
+    board;
